0b5dbb04ba5c1b1e5ace8c53f475244bd262131f
Arex360 <49303647+Arex360@users.noreply.github.com>
2024-05-13 17:20:11 +0500

    update

--- server.py
+++ server.py
@@ -5,28 +5,17 @@ import requests
 MQTT_BROKER = "152.67.19.152"  # Change this if your broker is on a different machine
 MQTT_PORT = 1883
 MQTT_TOPIC = "client9"
-a = ""
+
 # Callback when a message is received
-def on_message(client, userdata, msg):
-    a = a + "s"
+def on_messageStart(client, userdata, msg):
+    if "_0_" in msg.payload:
+        print("its start")
     print(len(msg.payload))
-    print(a)
-    #print(f"Received image from topic {msg.topic}")
-    #url = f'http://localhost:5000/postImage/{msg.topic}'
-    #myobj = {'base64': msg.payload,'client':msg.topic}
-    #x = requests.post(url, json = myobj)
-    # Decode base64-encoded image
-    #image_data = base64.b64decode(msg.payload)
-    
-    # Save the image to a file
-    #with open("received_image.jpg", "wb") as f:
-    #    f.write(image_data)
-    
-    #print("Image saved as received_image.jpg")
+
 
 # Create MQTT client
 client = mqtt.Client()
-client.on_message = on_message
+client.on_message = on_messageStart 
 client.connect(MQTT_BROKER, MQTT_PORT, 60)
 client.subscribe(MQTT_TOPIC)
 client.loop_forever()
