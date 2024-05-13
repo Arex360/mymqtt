f270066c291b5dd297d6a828f26727d667061500
Arex360 <49303647+Arex360@users.noreply.github.com>
2024-05-13 17:34:36 +0500

    update

--- server.py
+++ server.py
@@ -11,10 +11,20 @@ def on_messageStart(client, userdata, msg):
     a = str(msg.payload)
     if "_0_" in a:
         print("its start")
+        a = a.replace("_0_","")
+        myobj = {'base64': a}
+        requests.post(f'http://localhost:5000/postImagePart/{msg.topic}/0',myobj)
     if "_1_" in a:
         print("its mids")
+        a = a.replace("_1_","")
+        myobj = {'base64': a}
+        requests.post(f'http://localhost:5000/postImagePart/{msg.topic}/1',myobj)
     if "_2_" in a:
         print("its end")
+        a = a.replace("_2_","")
+        myobj = {'base64': a}
+        requests.post(f'http://localhost:5000/postImagePart/{msg.topic}/2',myobj)
+        print("request ended")
     print(len(a))
 
 
