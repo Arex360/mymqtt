89ad6ca89a5103205f4dbfd0b71ec64bd7bdec4c
Arex360 <49303647+Arex360@users.noreply.github.com>
2024-05-13 10:10:39 +0500

    update

---
+++ sender.py
@@ -0,0 +1,30 @@
+import paho.mqtt.publish as publish
+import base64
+
+# MQTT Settings
+MQTT_BROKER = "152.67.19.152"
+MQTT_PORT = 1883
+MQTT_TOPIC = "image_topic"
+
+# Read the image file
+def read_image_file(file_path):
+    with open(file_path, "rb") as f:
+        image_data = f.read()
+    return image_data
+
+# Encode the image data to base64
+def encode_image_to_base64(image_data):
+    return base64.b64encode(image_data)
+
+# Publish the image to the MQTT broker
+def publish_image(image_path):
+    image_data = read_image_file(image_path)
+    encoded_image = encode_image_to_base64(image_data)
+    publish.single(MQTT_TOPIC, encoded_image, hostname=MQTT_BROKER, port=MQTT_PORT)
+
+if __name__ == "__main__":
+    # Path to the image file
+    image_path = "01.png"
+    
+    # Publish the image to the MQTT broker
+    publish_image(image_path)
---
+++ server.py
@@ -0,0 +1,30 @@
+import paho.mqtt.client as mqtt
+import base64
+import requests
+# MQTT Settings
+MQTT_BROKER = "152.67.19.152"  # Change this if your broker is on a different machine
+MQTT_PORT = 1883
+MQTT_TOPIC = "client9"
+
+# Callback when a message is received
+def on_message(client, userdata, msg):
+    print(f"Received image from topic {msg.topic}")
+    url = f'http://localhost:5000/postImage/{msg.topic}'
+    myobj = {'base64': msg.payload,'client':msg.topic}
+    x = requests.post(url, json = myobj)
+    # Decode base64-encoded image
+    #image_data = base64.b64decode(msg.payload)
+    
+    # Save the image to a file
+    #with open("received_image.jpg", "wb") as f:
+    #    f.write(image_data)
+    
+    print("Image saved as received_image.jpg")
+
+# Create MQTT client
+client = mqtt.Client()
+client.on_message = on_message
+client.connect(MQTT_BROKER, MQTT_PORT, 60)
+client.subscribe(MQTT_TOPIC)
+client.loop_forever()
+
